1043599ad3f79511ca1a0cbf798ee106ebaf42da
laxman-taur9 <laxmantaur9@gmail.com>
2022-01-09 17:06:39 +0530

    Added:UC-2 The Player rolls the die

---
+++ src/com/Bridgelabz/SnakeandLadder.java
@@ -0,0 +1,34 @@
+package com.Bridgelabz;
+
+public class SnakeandLadder {
+
+	//Declaring Variables for Players Position
+    int playerPosition = 0;
+
+    //Method to Show Player Current Position
+    private void showPosition() {
+        //Displaying Player Position
+        System.out.println("Player Starting Position : "+ playerPosition);
+    }
+
+    //Method to Roll the Die and Get No between 1 to 6
+    private int rollDie() {
+        int dieNo = (int) Math.floor(Math.random() * 10)%6+1;
+        return dieNo;
+    }
+
+    public static void main(String[] args) {
+        //Displaying Welcome Message
+        System.out.println("Welcome to Snake and Ladder Simulator Problem Developed by Tahir Mansuri.");
+
+        //Creating Object for Player
+        SnakeandLadder player1 = new SnakeandLadder();
+
+        //Showing Player Position
+        player1.showPosition();
+
+        //Show Die Rolling No
+        System.out.println("Die Number for Player : "+ player1.rollDie());
+    }
+}
+
